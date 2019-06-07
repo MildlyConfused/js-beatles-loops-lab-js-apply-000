7fcdfc9231334a5db37c51de35b951f69ffa13ae
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-07 15:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(artists, instuments){
+  let arr = []
+  for(let i = 0; i < artists.length; i++){
+    let s = `${artists[i]} plays ${instuments[i]}`
+    arr.push(s)
+  }
+  return arr
+}
+
+function johnLennonFacts(facts){
+  let arr = []
+  for(let i = 0; i < facts.length; i++){
+    arr[i] = facts[i] + '!!!' 
+  }
+  return arr
+}
+
+function iLoveTheBeatles(num){
+  
+}
